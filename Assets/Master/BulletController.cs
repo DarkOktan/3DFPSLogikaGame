0ed93edbf5a98b319ba74a1286206e981ce2941c
LabKomp-5 <LabKomp5@gmail.com>
2022-11-17 15:14:35 +0700

    Upload Project
    UAS Logika Game

---
+++ Assets/Master/BulletController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+	public float angle;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position =  new Vector3(transform.position.x + Mathf.Cos(-(angle-90) * Mathf.Deg2Rad) * speed,
+		transform.position.y, transform.position.z + Mathf.Sin(-(angle-90) * Mathf.Deg2Rad) * speed);
+	}
+}
---
+++ Assets/Master/HeroController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ThrowProjectile();
+	}
+
+	void ThrowProjectile(){
+		if(Input.GetMouseButtonDown(0)){
+			GameObject bulletObject = Instantiate(bulletPrefab);
+			
+			BulletController bullet = bulletObject.GetComponent<BulletController>();
+
+			Debug.Log("LocalEulerAngles y: " + transform.localEulerAngles.y);
+			Debug.Log("\nRotation Y: " + transform.rotation.y);
+
+			bullet.angle = transform.localEulerAngles.y;
+
+			bulletObject.transform.position = new Vector3(transform.position.x + Mathf.Cos(-(bullet.angle-90) * Mathf.Deg2Rad) * 3,
+			transform.position.y, transform.position.z + Mathf.Sin(-(bullet.angle-90) * Mathf.Deg2Rad) * 3);
+		}
+	}
+}
